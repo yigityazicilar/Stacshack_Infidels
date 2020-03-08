6ccdfa82eb596c8e851b72a30d3c6c6d6fb83863
TheGreatTerminator <alexandre.guerville@hotmail.com>
2020-03-08 15:04:50 +0000

    Added WordDirector, and making game logic.

--- core/src/com/mygdx/game/Entity.java
+++ core/src/com/mygdx/game/Entity.java
@@ -1,24 +1,13 @@
 package com.mygdx.game;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
 public abstract class Entity {
     protected String name;
     protected int health = 100;
-    protected HashMap<String, Attack> attacks;
     protected double multiplier;
 
     public Entity(String name, Role role){
         this.name = name;
-        this.attacks = new HashMap<>();
         importRole();
-        importAttacks(role.getFilePath());
 
     }
 
@@ -31,15 +20,6 @@ public abstract class Entity {
     }
 
 
-    public int sendAttack(String word){
-        if(this.attacks.containsKey(word)){
-            System.out.println("word is contained");
-            return (int)Math.floor(word.length() * multiplier);
-        }else{
-            return 0;
-        }
-
-    }
 
     public void hurt(int damage){
         this.health -= damage;
@@ -56,23 +36,4 @@ public abstract class Entity {
     public void importRole(){
     }
 
-    public void importAttacks(String filepath){
-        try{
-            List<String> moves = Files.readAllLines(Paths.get(filepath));
-
-            String[] components;
-
-            for(int i = 0; i < moves.size(); i++){
-                components = moves.get(i).split(" ");
-                this.attacks.put(components[0], new Attack(components[0].length(),
-                                                Integer.parseInt(components[1]),
-                                                Integer.parseInt(components[2])));
-            }
-
-        }catch(IOException e){
-            System.out.println("Cannot get " + e.getMessage());
-        }
-
-
-    }
 }
---
+++ core/src/com/mygdx/game/GameDirector.java
@@ -0,0 +1,140 @@
+package com.mygdx.game;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.TimerTask;
+
+import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+
+public class GameDirector
+{
+	List<String> playerWordList;
+	List<String> enemyWordList;
+
+	String[] currentWordList;
+	Entity target;
+
+	boolean isPlayerTurn;
+	boolean wordIsComplete;
+
+	Timer roundTimer;
+	TimerTask roundTask = new TimerTask(){
+
+		@Override
+		public void run() {
+			endTimer();
+		}
+	};
+	public String[] requestEnemyWord(int wordDifficulty, int numberOfWords, int duration, Entity target)
+	{
+		// Initialize vars
+		int[] wordDims = new int[] {Math.max(2 ,wordDifficulty-2), Math.max(2, wordDifficulty + 2)};
+		String[] enemyWordsArray = new String[numberOfWords];
+		for (int i = 0; i < enemyWordsArray.length; i++) 
+			enemyWordsArray[i] = "";
+
+		// Choose random word.
+		for(int i = 0; i < enemyWordsArray.length; i++)
+		{
+			boolean wordListSet = false;
+			while(!wordListSet)
+			{
+				int randomIndex = new Random().nextInt(enemyWordList.size() - 1);
+				String randoWord = enemyWordList.get(randomIndex);
+
+				// Check if word respects difficulty
+				if (wordDims[0] <= randoWord.length() && wordDims[1] >= randoWord.length())
+					wordListSet = true;
+
+				// Check for duplicates
+				for (String nextString: enemyWordsArray) 
+					if(nextString.equals(randoWord))
+						wordListSet = false;
+			}
+		}
+
+		// Set timer
+		startRoundTimer(duration);
+		currentWordList = enemyWordsArray;
+		return enemyWordsArray;
+	}
+
+	public String[] requestPlayerWordBatch()
+	{
+		String[] playerWordsArray = new String[] {"","",""};
+		
+		for(int i = 0; i < playerWordsArray.length; i++)
+		{
+			boolean wordIsSet = false;
+			String randoWord = "";
+			while(!wordIsSet)
+			{
+				int randomIndex = new Random().nextInt(playerWordList.size() - 1);
+				randoWord = playerWordList.get(randomIndex);
+				wordIsSet = true;
+
+				// Check for duplicates
+				for (String nextString: playerWordsArray)
+					if (nextString.equals(randoWord))
+						wordIsSet = false;
+			}
+			playerWordsArray[i] = randoWord;
+		}
+
+		startRoundTimer(3);
+		currentWordList = playerWordsArray;
+		return playerWordsArray;
+	}
+
+	public void startRoundTimer(int duration)
+	{
+		roundTimer = new Timer(true);
+		wordIsComplete = false;
+		roundTimer.schedule(roundTask, duration * 1000);
+	}
+
+	public void endTimer()
+	{
+		if(!isPlayerTurn && !wordIsComplete)
+		{
+			int damage = 0;
+			for (String nextString: currentWordList) damage += nextString.length();
+			target.hurt(damage);
+		}
+		if(isPlayerTurn && wordIsComplete)
+		{
+			int damage = 0;
+			for (String nextString: currentWordList) damage += nextString.length();
+			target.hurt(damage);
+		}
+
+
+	}
+
+	public void sendWordsComplete()
+	{
+		wordIsComplete = true;
+	}
+
+	public double getTimeUntilEndOfTimer()
+	{
+		return roundTask.scheduledExecutionTime();
+	}
+
+	public GameDirector()
+	{
+		try
+		{
+			playerWordList = Files.readAllLines(Paths.get(Role.FIGHTER.getFilePath()));
+			enemyWordList = Files.readAllLines(Paths.get(Role.BOSS.getFilePath()));
+		}
+		catch(IOException e)
+		{
+			System.out.println("Cannot get file! System abort!");
+		}
+	}
+}
